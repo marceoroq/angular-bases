f51ac348bb751f4f7cff7718d65cc1e910cb4dd8
Marcelo Oroquieta <oroquieta.m@gmail.com>
2024-11-15 22:04:45 -0300

    feat(DbzService): add dbz service

---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { v4 as uuid } from 'uuid';
+
+import { Character } from '../interfaces/character.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DbzService {
+  constructor() {}
+
+  private characters: Character[] = [
+    {
+      id: uuid(),
+      name: 'Krillin',
+      power: 1000,
+    },
+    {
+      id: uuid(),
+      name: 'Goku',
+      power: 9500,
+    },
+    {
+      id: uuid(),
+      name: 'Vegetas',
+      power: 7500,
+    },
+  ];
+
+  public getCharacters(): Character[] {
+    return [...this.characters];
+  }
+
+  public addCharacter(character: Character) {
+    const newCharacter: Character = { id: uuid(), ...character };
+    this.characters.push(newCharacter);
+  }
+
+  public deleteCharacter(itemId: string) {
+    this.characters = this.characters.filter(
+      (character) => character.id !== itemId
+    );
+  }
+}
